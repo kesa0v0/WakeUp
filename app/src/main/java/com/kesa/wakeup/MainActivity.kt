e4d93a72b9d314ddff3a6ae9c9bf97c9ec5fa35e
kesa0v0 <kesa0v0@users.noreply.github.com>
2020-11-16 20:17:01 +0900

    zz

--- app/src/main/java/com/kesa/wakeup/MainActivity.kt
+++ app/src/main/java/com/kesa/wakeup/MainActivity.kt
@@ -9,13 +9,15 @@ import android.os.*
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import kotlin.math.absoluteValue
 import kotlin.math.pow
 import kotlin.math.sqrt
 
 
 class MainActivity : AppCompatActivity() {
-
-    private val sensorThreshold = 5
+    private val timeInterval : Long = 50 * 100
+    private val sensorThreshold = 0.3
+    lateinit var countDownTimer:CountDownTimer
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,10 +25,11 @@ class MainActivity : AppCompatActivity() {
 
         val sensorWork = SensorWorks()
 
-        val showTime = findViewById<TextView>(R.id.textView)
-        val x = findViewById<TextView>(R.id.x)
-        val y = findViewById<TextView>(R.id.y)
-        val z = findViewById<TextView>(R.id.z)
+        val showTime = findViewById<TextView>(R.id.TimeLeftByChecking)
+        val x = findViewById<TextView>(R.id.AccX)
+        val y = findViewById<TextView>(R.id.AccY)
+        val z = findViewById<TextView>(R.id.AccZ)
+        val tot = findViewById<TextView>(R.id.AccTotal)
 
         val turnOnBtm = findViewById<Button>(R.id.recordOn)
         val turnOffBtn = findViewById<Button>(R.id.recordOff)
@@ -35,63 +38,83 @@ class MainActivity : AppCompatActivity() {
         val sensor: Sensor? = sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION)
         sensorManager.registerListener(sensorWork, sensor, SensorManager.SENSOR_DELAY_NORMAL)
 
-        val mHideHandler = Handler(Looper.getMainLooper())
-        mHideHandler.post {
 
+        val handler = Handler()
+        var trigger = true
+        val obj = Runnable {
+            if (trigger) {
+                sensorWork.max = Float.MIN_VALUE
+                countDownTimer.start()
+            }
         }
-        mHideHandler.removeCallbacksAndMessages(null);
+        countDownTimer = object : CountDownTimer(timeInterval, 10) {
 
-        val countDownTimer: CountDownTimer = object : CountDownTimer(2000 * 10, 10) {
-
-            var localMax:Float = Float.MIN_VALUE
             override fun onTick(millisUntilFinished: Long) {
                 //반복실행할 구문
-                if (localMax < sensorWork.lTotal) localMax = sensorWork.lTotal
 
                 showTime.text = millisUntilFinished.toString()
                 x.text = sensorWork.lAccX.toString()
                 y.text = sensorWork.lAccY.toString()
                 z.text = sensorWork.lAccZ.toString()
+                tot.text = sensorWork.lTotal.toString()
             }
 
             override fun onFinish() {
                 //마지막에 실행할 구문
-                if (localMax > sensorThreshold) {
-                    DoAsync{vibrationOn()}
-                    showTime.text = localMax.toString()
+                if (sensorWork.max< sensorThreshold) {
+                    vibrationOn()
+                    showTime.text = sensorWork.max.toString()
                 }
+
+                handler.post(obj)
             }
         }
 
+
         turnOnBtm.setOnClickListener{
-            countDownTimer.start() //CountDownTimer 실행
+            trigger = true
+            handler.post(obj)
+//            countDownTimer.start() //CountDownTimer 실행
         }
         turnOffBtn.setOnClickListener{
-            countDownTimer.cancel() // 타이머 종료
+            trigger = false
+//            countDownTimer.cancel() // 타이머 종료
         }
 
     }
 
     private fun vibrationOn() {
         val vib = getSystemService(Context.VIBRATOR_SERVICE) as Vibrator  // 진동
-        vib.vibrate(VibrationEffect.createWaveform(longArrayOf(500, 1000, 500, 2000), 2))
+        vib.vibrate(VibrationEffect.createWaveform(longArrayOf(0, 100, 100, 100, 100), intArrayOf(0, 100, 0, 100, 0), -1))
     }
 
 }
 
 class SensorWorks : SensorEventListener{
+    var preX: Float = 0.0f
+    var preY: Float = 0.0f
+    var preZ: Float = 0.0f
+
     var lAccX: Float = 0.0f
     var lAccY: Float = 0.0f
     var lAccZ: Float = 0.0f
 
     var lTotal: Float = 0.0f
 
+    var max = Float.MIN_VALUE
+
     override fun onSensorChanged(event: SensorEvent){
-        lAccX = event.values[0]
-        lAccY = event.values[1]
-        lAccZ = event.values[2]
+        lAccX = event.values[0].absoluteValue - preX
+        lAccY = event.values[1].absoluteValue - preY
+        lAccZ = event.values[2].absoluteValue - preZ
+
+        preX = event.values[0].absoluteValue
+        preY = event.values[1].absoluteValue
+        preZ = event.values[2].absoluteValue
 
         lTotal = sqrt(lAccX.pow(2) + lAccY.pow(2) + lAccZ.pow(2))
+
+        if (max < lTotal) max = lTotal
     }
 
     override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
